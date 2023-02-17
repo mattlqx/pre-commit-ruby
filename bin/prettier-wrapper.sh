8499b7a4c8f66f00b79dfdcf1d188ffad27318ab
RafeSl <rafe.slattery@clio.com>
2023-02-17 10:38:58 +0000

    add prettier wrapper

---
+++ bin/prettier-wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+trap 'print_error' ERR
+
+if [[ $(git log -1 --pretty=%B) == Revert* ]]
+then
+  echo "Not running prettier because this looks like a revert commit, and we would"
+  echo "prefer to let reverts into production without subjecting them to formatting, "
+  echo "because a commit that fixed some formatting could also have introduced a bug"
+  exit 0
+fi
+
+diff=$(script/ci/list_changed_files --diff-filter=ACMRXB | grep "components/manage/" | sed 's:^components/manage/::' | xargs)
+
+if [ -z "$diff" ]
+then
+  echo "No matching files - skipping step."
+else
+  yarn --cwd "components/manage" prettier --check "$diff"
+fi
